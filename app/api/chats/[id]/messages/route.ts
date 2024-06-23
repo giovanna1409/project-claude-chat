6a85e989b0b7cc77146209fa4c575b2634528fad
giovanna1409 <giovannadossantoscarvalho@usp.br>
2024-06-23 19:28:22 -0300

    adjust the use of anthopic api

--- app/api/chats/[id]/messages/route.ts
+++ app/api/chats/[id]/messages/route.ts
@@ -1,17 +1,13 @@
 import { addAnswer, addMessage, getChatMessages } from "../../../../../lib/data";
 import { NextResponse } from "next/server";
-import Anthropic from "@anthropic-ai/sdk";
-
-const anthropic = new Anthropic({
-    apiKey: "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",
-});
+import axios from "axios";
 
 export const GET = async (req: Request) => {
     try {
         const id = req.url.split("chats/")[1].split("/")[0];
         const chatMessages = await getChatMessages(id);
 
-        if (!chatMessages) {
+        if (chatMessages == null || undefined) {
             throw new Error('No messages found');
         }
 
@@ -35,24 +31,25 @@ export const POST = async (req: Request) => {
         const id = req.url.split("chats/")[1].split("/")[0];
         const chatMessage = { id_chat: id, content };
         await addMessage(chatMessage);
-        const answer = await anthropic.messages.create({
+        const answer = await axios.post('https://api.anthropic.com/v1/messages', {
             model: "claude-3-opus-20240229",
             max_tokens: 1000,
             temperature: 0,
             system: "You give me contextualized answers.",
             messages: [
                 {
-                    "role": "user",
-                    "content": [
-                        {
-                            "type": "text",
-                            "text": chatMessage.content
-                        }
-                    ]
+                    role: "user",
+                    content: chatMessage.content
                 }
             ]
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': process.env.ANTHROPIC_API_KEY,
+                'anthropic-version': '2023-06-01'
+            }
         });
-        const chatAnswer = { id_chat: id, content: answer.content[0].text };
+        const chatAnswer = { id_chat: id, content: answer.data.content[0].text };
         await addAnswer(chatAnswer);
         return NextResponse.json({ message: "Ok", chatAnswer }, {
             status: 201,
